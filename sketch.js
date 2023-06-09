793e65d0d8776a7b958721dd2aa6937bb37928dd
TashaTan1 <136006359+TashaTan1@users.noreply.github.com>
2023-06-09 20:16:13 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -89,7 +89,7 @@ function draw() {
     button.position(200, height/2);
     button.mousePressed(function goToAnotherPage() {
       window.location.href =
-        "https://editor.p5js.org/natashatan/sketches/aQSdgJcCR";
+        "https://tashatan1.github.io/visual-eight/";
     });
     button = createButton("Let's Breath");
     button.position(425, height/2);
